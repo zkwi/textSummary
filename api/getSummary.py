ba0badc4a9271f5e3edca91d2554828beb6e1c93
zkwi2010 <zkwi2010@gmail.com>
2016-03-18 08:57:04 +0800

    0.01版本
    使用简单的tf-idf、贪婪算法完成摘要提取，前端使用angularjs完成。

---
+++ api/getSummary.py
@@ -0,0 +1,117 @@
+#encoding=utf-8
+import jieba
+import jieba.analyse 
+# 读取文档
+def getSummary(text):
+	#　text = open("api//data4.txt", encoding='utf-8').read()
+	# 对文档进行分词、分句
+	jieba.analyse.set_stop_words("api//stop_words.txt")
+	words_generator = jieba.cut(text, cut_all=False)
+	words_list = list()
+	sentences = list() # 分词后的句子集
+	sentences_filter = list() # 去掉停止后的句子集合
+	sentences_filter_unique = list() # 对sentences_filter中的每个句子去重后的集合
+	sentence = list() # 包含停止词
+	sentence_filter = list() # 去掉停止词
+	sentence_stopwords = [ ".", "!", "?",  "。", "！", "？", "\n"] #分句
+	stopwords = list()
+	for line in open("api//stop_words.txt", encoding='utf-8').readlines():
+		stopwords.append(line.strip('\n'))
+	for word in words_generator:
+		if word in sentence_stopwords:
+			# 遇到句子分隔符，则拆成一个句子
+			sentences.append(sentence)
+			sentences_filter.append(sentence_filter)
+			sentences_filter_unique.append(list(set(sentence_filter)))
+			sentence = []
+			sentence_filter =[]
+		else:
+			words_list.append(word)
+			sentence.append(word)
+			if word not in stopwords:
+				sentence_filter.append(word)
+	# 提取tf-idf值
+	tf_idfs = jieba.analyse.extract_tags(text, topK=len(words_list), withWeight=True)
+	# 计算SR的值
+	srs = list()
+	for sentence_filter_unique in sentences_filter_unique:
+		sum = 0
+		for word in sentence_filter_unique:
+			for t in tf_idfs:
+				if t[0] == word:
+					sum = sum + t[1] # tf_idfs值求和
+		if len(sentence_filter_unique) == 0:
+			sr = 0
+		else:
+			sr = sum
+		srs.append(sr)
+	# 将分词后的sentence，去重后的tr，相关性sr整理在一起
+	document = list()
+	for i in range(len(sentences)):
+		# 去除0分项
+		if srs[i] == 0:
+			continue
+		# 去重
+		existd = False
+		for d in document:
+			if sentences[i] == d["sentence"]:
+				existd = True
+				break
+		if existd:
+			continue
+		sentence = dict()
+		sentence["sentence"] = sentences[i]
+		sentence["sentence_join"] = "".join(sentences[i])
+		sentence["sentence_filter"] = sentences_filter[i]
+		# 此处，原算法是加权值，但我认为加权之后偏向于输出短句，效果不好，因此修改为不加要权
+		# sentence["SR"] = srs[i] / len(sentence["sentence"])
+		sentence["SR"] = srs[i] # 相关性
+		document.append(sentence)
+
+	document = sorted(document, key=lambda k: k['SR'], reverse=True) # 按SR也就是相关性进行倒序
+	# 计算频繁项集
+	from pymining import itemmining
+	item_sets = itemmining.relim(itemmining.get_relim_input(sentences_filter), min_support=2)
+	# 合并频繁项集D(I)item_sets
+	D = set()
+	D_temp = set() # 创建一个临时变量，存储超过2个字符的词
+	for item in item_sets:
+		D = D | item # 进行并集计算
+	for i in D:
+		if len(i) >= 2:
+			D_temp.add(i)
+	# 计算tf_idfs值，取出排名靠前的20个词
+	words_text = " ".join(D_temp)
+	tf_idfs = jieba.analyse.extract_tags(words_text, topK=len(words_list), withWeight=True)
+	D_temp = set()
+	for t in tf_idfs:
+		if len(D_temp) <= 20:
+			D_temp.add(t[0])
+	import copy
+	D = copy.deepcopy(D_temp)
+	print("通过频繁项集计算出来的词组", D)
+	# 通过贪心算法计算
+	for sentence in document:
+		sentence["SC"] = 0
+		for word in sentence["sentence"]:
+			if word in D_temp:
+				sentence["SC"] = 1
+				D_temp.remove(word)
+	print("")
+	print("摘要句子, 相关性")
+	# 提取出关键句，并且要求句子由至少3个词组成
+	results = dict()
+	results["keywords"] = list(D)
+	results["sentences"] = list()
+	for sentence in document:
+		if sentence["SC"] == 1 and len(sentence["sentence"]) > 2:
+			results_sentence = dict()
+			results_sentence["SR"] = round(sentence["SR"], 2)
+			results_sentence["sentence_join"] = sentence["sentence_join"]
+			results["sentences"].append(results_sentence)
+			print(sentence["sentence_join"], sentence["SR"])
+	return results
+'''
+r = getSummary(open("api//data.txt", encoding="utf-8").read())
+print(r)
+'''
---
+++ web.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/api/getSummary/', methods=['GET', 'POST'])
+def hello_world():
+	content = dict()
+	try:
+		content = request.json
+	except:
+		return "error:-3 请求数据格式错误"
+	if not type(content) == dict:
+		return "error:-2 请求json格式错误"
+	if 'text' not in content:
+		return "error:-1 text字段为空"
+	text = content['text']
+	import api.getSummary
+	results = api.getSummary.getSummary(text)
+
+	return 	json.dumps(results)
+
+@app.route('/')
+def index():
+	text = open("api//data4.txt", encoding='utf-8').read()
+	source = dict()
+	source["text"] = text
+	results = json.dumps(source)
+	# 不渲染变量，直接输出模板，以免和angularjs冲突
+	return app.send_static_file("index.html")
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
