5fa010eff94d8daaec86cf6cf7adf92d35c593ab
zkwi2010 <zkwi2010@gmail.com>
2016-03-23 17:57:43 +0800

    增加requirements.txt，尝试配置heroku 0.07

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 import api.getSummary
-text = open("api/data.txt", encoding="utf-8").read()
+text = open("testdata/data.txt", encoding="utf-8").read()
 title = "习近平和彭丽媛出席英国女王伊丽莎白二世举行的欢迎晚宴"
 results = api.getSummary.getSummary(text, title)
 print(results)
 
-text = open("api/data1.txt", encoding="utf-8").read()
+text = open("testdata/data1.txt", encoding="utf-8").read()
 title = "打好全面建成小康社会决胜之战"
 results = api.getSummary.getSummary(text, title)
 print(results)
 
-text = open("api/data2.txt", encoding="utf-8").read()
+text = open("testdata/data2.txt", encoding="utf-8").read()
 title = "两位老部长释疑当你老了怎么办"
 results = api.getSummary.getSummary(text, title)
 print(results)
 
-text = open("api/data3.txt", encoding="utf-8").read()
+text = open("testdata/data3.txt", encoding="utf-8").read()
 title = "外媒关注中国老龄化：每年新增墓地相当1/3巴黎"
 results = api.getSummary.getSummary(text, title)
 print(results)
 
-text = open("api/data4.txt", encoding="utf-8").read()
+text = open("testdata/data4.txt", encoding="utf-8").read()
 title = "美国国会挺台入国际刑警组织 呼吁奥巴马签署法案"
 results = api.getSummary.getSummary(text, title)
 print(results)
